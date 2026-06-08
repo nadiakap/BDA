bf195e865a41a31af0bc8413251179e411066862
nadiakap <nadiakap@optonline.net>
2026-06-08 17:57:53 -0400

    sql query as alternative to mapreduce - example using taxi_trips datasrt

---
+++ sqlite_txi_demo2.m
@@ -0,0 +1,34 @@
+% 1. Load the CSV file into a MATLAB table
+% (Assuming 'pickup_datetime' and 'tip_amount' are in the CSV)
+%taxiData = readtable('taxi_data_subset.csv');
+
+% 2. Create and connect to an SQLite database file
+%bfile = 'nyctaxi.db';
+%conn = sqlite(dbfile, 'create');
+
+% 3. Write the MATLAB table to the SQLite database
+%tableName = 'taxi_trips';
+%sqlwrite(conn, tableName, taxiData);
+
+dbfile = 'nyctaxi.db';
+conn = sqlite(dbfile, 'connect');
+
+% 1. Construct the SQL query to calculate average tip per hour
+sqlQuery = sprintf([...
+    'SELECT strftime(''%%H'', tpep_pickup_datetime) AS trip_hour, ' ...
+    'AVG(tip_amount) AS avg_tip ' ...
+    'FROM %s ' ...
+    'GROUP BY trip_hour ' ...
+    'ORDER BY trip_hour ASC'], tableName);
+
+% 2. Execute the query and store the results in a MATLAB table
+hourlyTips = fetch(conn, sqlQuery);
+
+% 3. View or plot the results
+disp(hourlyTips);
+bar(str2double(hourlyTips.trip_hour), hourlyTips.avg_tip);
+xlabel('Hour of the Day');
+ylabel('Average Tip ($)');
+
+% 4. Close the connection when done
+close(conn);
